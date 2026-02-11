400c6c3a24361d3a6becc4af38b52092b6e2e52a
ajrajput-04 <ajaysinghajaymanhas@gmail.com>
2026-02-11 23:49:22 +0530

    find the bulbs which or turned on at Nth iteration for n bulbs

---
+++ banjamin_bulb.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+class banjamin_bulb {
+    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+for(int i =1; i*i<=n; i++){
+    System.out.println(i*i);
+}
+
+    }
+}
