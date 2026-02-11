7be9449cf7bbbdf28dd3447ff77fd230762706d8
Ajay Singh <ajaysinghajaymanhas@gmail.com>
2026-02-11 23:52:34 +0530

    Fix loop structure to avoid redundancy
    Removed redundant inner loop for printing squares.

--- banjamin_bulb.java
+++ banjamin_bulb.java
@@ -5,9 +5,8 @@ class banjamin_bulb {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-for(int i =1; i*i<=n; i++){
+    for(int i =1; i*i<=n; i++){
     System.out.println(i*i);
-}
-
     }
-}
+   }
+ }
