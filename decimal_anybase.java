56431d481242e946e5db5b1d0a1038e1be919fa5
ajrajput-04 <ajaysinghajaymanhas@gmail.com>
2026-02-14 21:09:58 +0530

    Update project files

--- decimal_anybase.java
+++ decimal_anybase.java
@@ -1,35 +1,7 @@
-<<<<<<< HEAD
+
 import java.util.*;
 import java.util.Scanner;
-public class decimal_anybase {
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in) ;
-
-    int n = sc.nextInt();
-    int b = sc.nextInt();
-    int dn = getValueInBase(n,b);
-    System.out.println(dn);
-
-    }
-     public static int getValueInBase(int n, int b)
-    {
-        int p = 1;
-        int rv = 0;
-        while (n>0){
-            int dig = n% b;
-            n = n/b;
-            rv += dig *p;
-             p = p*10;
-        }
-        return rv;
-
-    }
-    
-}
-=======
-import java.util.*;
-import java.util.Scanner;
-public class decimal_anybase {
+ class decimal_anybase {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in) ;
 
@@ -55,4 +27,3 @@ public class decimal_anybase {
     }
     
 }
->>>>>>> 1dd8529 (save changes before pull)
